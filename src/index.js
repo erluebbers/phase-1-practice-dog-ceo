9ead1dc5d50aabbb42a7a9b4201ab7f11dae1b4c
Erik Luebbers <erluebbers@gmail.com>
2021-11-20 09:19:41 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            for (const source of data) {
+                
+            }
+        })
+})
+
+// let newImage = document.createElement('img')
+// newImage.src = element
