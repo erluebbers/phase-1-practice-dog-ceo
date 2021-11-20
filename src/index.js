0fa5be35358c87fb1ac1cc408787dc7ef16b2274
Erik Luebbers <erluebbers@gmail.com>
2021-11-20 11:44:43 -0800

    commit tree a9412054645495e83da1db4050124e43854622bf

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 // console.log('%c HI', 'color: firebrick')
 
+const dogBreeds = []
+
 document.addEventListener('DOMContentLoaded', () => {
     fetch("https://dog.ceo/api/breeds/image/random/4")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
-            for (const source of data) {
-                
-            }
+            data.message.forEach((element) => {
+                let newImage = document.createElement('img')
+                newImage.src = element
+                document.querySelector('#dog-image-container').appendChild(newImage)
+            })
+        })
+    fetch("https://dog.ceo/api/breeds/list/all")
+        .then(res => res.json())
+        .then(data => {
+            let breeds = Object.keys(data.message)
+            dogBreeds.push(...breeds)
+            breeds.forEach(element => {
+                let newLi = document.createElement('li')
+                newLi.innerText = element
+                document.querySelector('#dog-breeds').appendChild(newLi)
+                newLi.addEventListener('click', () => {
+                    newLi.style.color = "Crimson"
+                })
+            })
+            const dropDown = document.querySelector('#breed-dropdown')
+            dropDown.addEventListener('change', (event) => {
+                let sortedArray = dogBreeds.filter((element) => {
+                    return element.charAt(0) === event.target.value
+                })
+                console.log(sortedArray)
+            })
         })
 })
-
-// let newImage = document.createElement('img')
-// newImage.src = element
